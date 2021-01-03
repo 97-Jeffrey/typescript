6fb3ec0bc60cea48a1e4b1ecaae47273df0b73e0
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-01-03 00:32:07 -0800

    some practices done with typescript

---
+++ ts-hello/main.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+var count = 5;
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3, 4, 5];
+var f = [1, true, 'kjdxfe', null];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Blue;
+console.log(backgroundColor);
---
+++ ts-hello/main.ts
@@ -0,0 +1,20 @@
+export {};
+
+
+let count =5;
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[]=[1,2,3,4,5];
+let f: any[]=[1, true, 'kjdxfe', null];
+
+const ColorRed =0;
+const ColorGreen=1;
+const ColorBlue=2; 
+
+enum Color { Red, Green, Blue };
+let backgroundColor=Color.Blue;
+console.log(backgroundColor)
+
